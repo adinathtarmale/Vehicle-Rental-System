0c640067c4f0e38c1e9c4afdbfd2f5917688b0cd
adinathtarmale <59080897+adinathtarmale@users.noreply.github.com>
2022-04-19 10:09:00 +0530

    Update UserService.js

--- vehicle-rentle-system-frontend/src/services/UserService.js
+++ vehicle-rentle-system-frontend/src/services/UserService.js
@@ -1,30 +1,4 @@
-//import axios from 'axios';
 import httpClient from '../http-common';
-
-//const USER_GET_ALL_VEHICLES = "http://localhost:9090/homePage";
-
-//const USER_REGISTRATION = "http://localhost:9090/userRegistration";
-
-//const USER_LOGIN_URL = "http://localhost:9090/userLogin";
-
-//-------------------------------- trying to use class components------------------------------
-//export default class UserService {
-
-   // getAllVehicles(){
-     //   return httpClient.get(USER_GET_ALL_VEHICLES);
-    //}
-    
-    // userLoginFront(user){
-    //     return httpClient.post('/userLogin', user);
-    // }
-
-    // userLoginFront(userEmail, userPassword) {
-    //     return axios.post('http://localhost:9090/userLogin', {
-    //         userEmail: userEmail,
-    //         userPassword: userPassword,
-    //     });
-    // }
-//}
 
 const getAllVehicles = () => {
     return httpClient.get('http://localhost:9090/homePage');
